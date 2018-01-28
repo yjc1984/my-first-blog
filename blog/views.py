c0ea2bd47ef44a2c22fbec3bce5c9f71e42579f9
Yeison <yjc1984@gmail.com>
2018-01-28 09:16:38 -0500

    Cambie el HTML para la página.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+#método (def) llamado post_list que toma un request y hace un return de un método render que renderizará (construirá)
+#nuestra plantilla blog/post_list.html.
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
--- mysite/urls.py
+++ mysite/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    # para cada URL que empieza con admin/ Django encontrará su correspondiente view
     url(r'^admin/', include(admin.site.urls)),
+    url(r'', include('blog.urls')),
 ]
